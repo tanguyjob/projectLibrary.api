89b818b4ea2d4a92a6e1b6b4ba85cbcc37f00b54
Tanguy <tanguy_job@hotmail.fr>
2022-06-30 15:58:24 +0200

    creation DAL BorrowingBookById

---
+++ ProjectLibraryApiInMultiTier/ProjectLibraryApiInMultiTier.DAL/Entities/BorrowingBookCustomEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibraryApiInMultiTier.DAL.Entities
+{
+    public class BorrowingBookCustomEntity
+    {
+        public int BorrowingId { get; set; }
+        public DateTime DateBack { get; set; }
+        public DateTime DateBegin { get; set; }
+        public bool Additional { get; set; }
+        public int CopyId { get; set; }
+        public string title { get; set; }
+        public string Resume { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
---
+++ ProjectLibraryApiInMultiTier/ProjectLibraryApiInMultiTier.DAL/Repositories/BorrowingBookCustomRepository.cs
@@ -0,0 +1,69 @@
+﻿using ProjectLibraryApiInMultiTier.DAL.Entities;
+using ProjectLibraryApiInMultiTier.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Connections;
+
+namespace ProjectLibraryApiInMultiTier.DAL.Repositories
+{
+    public class BorrowingBookCustomRepository : IBorrowingBookCustom
+    {
+        protected Connection _Connection { get; set; }
+
+        public BorrowingBookCustomRepository(Connection connection)
+        {
+            _Connection = connection;
+         
+        }
+
+        public override IEnumerable<BorrowingBookCustomEntity> GetById(int id)
+        {
+            Command cmd = new Command($"SelectAllInfoBorrowingByUserId", true);
+            cmd.AddParameter("Id", id);
+
+            return _Connection.ExecuteReader(cmd, MapRecordToEntity);
+        }
+
+        private BorrowingBookCustomEntity MapRecordToEntity(IDataRecord arg)
+        {
+            // A compléter
+            return new AuthorEntity()
+            {
+                Id = (int)record[TableId],
+                Name = (string)record["Name"],
+                Firstname = (string)record["Firstname"],
+                Birthdate = (DateTime)record["Birthdate"],
+
+            };
+        }
+
+        public int Insert(BorrowingBookCustomEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        BorrowingBookCustomEntity IRepository<int, BorrowingBookCustomEntity>.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BorrowingBookCustomEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, BorrowingBookCustomEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
