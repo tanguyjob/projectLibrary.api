7eec947ca3632a20a61da21baa7ec6fc2f4cd1c9
Tanguy <tanguy_job@hotmail.fr>
2022-07-25 15:48:58 +0200

    manage the cors policy

--- ProjectLibraryApiInMultiTier/ProjectLibraryApiInMultiTier/Program.cs
+++ ProjectLibraryApiInMultiTier/ProjectLibraryApiInMultiTier/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:4200");
+                          policy.AllowAnyHeader();
                       });
 });
 
